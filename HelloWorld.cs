c8c5297d45ea8c5128ab6477ce23781f2e9279a5
Aziiid7 <azidab3@gmail.com>
2025-02-12 04:58:26 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour  // Inherit from MonoBehaviour
+{
+    // Called when the script is first run
+    void Start()
+    {
+        // Display a message in the console
+        print("Hello, World!");
+    }
+
+    // Called every frame
+    void Update()
+    {
+        // Optional: Do something every frame
+    }
+}
